cc21b4bb290a96b6917aa1dedcf988334070d6d4
Thiago Machado de Azevedo <thiago_machado_azevedo@hotmail.com>
2020-04-08 01:17:51 -0300

    changed index file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import FrontPage from './components/App/FrontPage';
+import FrontPage from './components/FrontPage/FrontPage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
