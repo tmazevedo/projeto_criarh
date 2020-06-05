f8517256a009e10befb5e4671f38d20553c11d23
biawar <bianca.guerra@ci.ufpb.br>
2020-06-05 02:08:39 -0300

    put the footer and header on default layout

--- frontend/src/pages/_layouts/default/index.js
+++ frontend/src/pages/_layouts/default/index.js
@@ -38,10 +38,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+import FrontPageHeader from '../../../components/FrontPageHeader';
+import BlackFooter from '../../../components/BlackFooter';
+
 import { Wrapper } from './styles';
 
 export default function AuthLayout({children}){
-  return <Wrapper>{children}</Wrapper>;
+  return <Wrapper>
+    <FrontPageHeader />
+    {children}
+    <BlackFooter />
+    </Wrapper>;
 }
 
 AuthLayout.propTypes = {
