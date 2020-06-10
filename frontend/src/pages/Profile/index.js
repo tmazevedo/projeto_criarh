6c852d105f219c2d6d6838589183d5b014f2ff24
Thiago Azevedo <thiago_machado_azevedo@hotmail.com>
2020-06-09 23:48:46 -0300

    profile styles

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { useAuth0 } from '../../react-auth0-spa';
+import TextField from '@material-ui/core/TextField';
 // import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const { loading, user } = useAuth0();
+
+  return (
+    <div style={{ backgroundColor: 'rgb(242, 247, 255)', height: '720px' }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div style={{ backgroundColor: "#ffff", paddingTop: '10 %', width: '55%', margin: 'auto', overflow: 'visible', height: "67%" }}>
+        <br />
+        <br />
+        <div style={{ marginLeft: "62px" }}>
+          <b style={{verticalAlign: '-11px'}}>Nome Completo</b>  <TextField id="nameProfile" value={user.name} />
+        </div>
+
+        <img src={user.picture}
+          alt="imageProfile" style={{
+            borderRadius: '4px',
+            padding: '11px',
+            width: '150px',
+            margin: '70px',
+            
+          }} />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
