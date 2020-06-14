ad42d2df9bbce31c9c00b6e2fa5b94f99fe6b6cd
Orlando <orlando_uchoa@hotmail.com>
2020-06-14 16:56:27 -0300

    teste

--- src/pages/Profile/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function Profile() {
-  return <h1>Profile</h1>;
-}
-
-export default Profile;
--- src/pages/SignUp/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function SignUp() {
-  return <h1>Sign Up</h1>;
-}
-
-export default SignUp;
